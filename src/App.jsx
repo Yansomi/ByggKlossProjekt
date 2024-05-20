4ee2b6ab22ec4ef96dd1639f4e4f23be605aafbe
Yansomi <Hampus.93.Nilsson@gmail.com>
2024-05-20 19:01:29 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-
+import { Canvas } from '@react-three/fiber'
+import {Model} from '.assets'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      
+      <Canvas>
+        
+      </Canvas>
     </>
   )
 }
