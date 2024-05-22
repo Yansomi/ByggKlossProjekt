f844303487a299bdb014239a81916b0e56faa897
Yansomi <38041910+Yansomi@users.noreply.github.com>
2024-05-22 13:02:18 +0200

    added camera control and simple background

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react';
 import './App.css';
 import { Canvas } from '@react-three/fiber';
-import {Model} from '../src/assets/Scene'
+import {Model} from '../src/assets/Scene';
+import { OrbitControls } from '@react-three/drei';
+
 function App() {
   return (
     <>
-      <Canvas>
-        <Model/>
+      <Canvas camera={{fov: 60, near: 0.1, far: 1000}}>
+        <OrbitControls/>
+        <ambientLight intensity={0.5}/>
+        <spotLight position={[-20, -20, -30]} angle={0.15} penumbra={1}/>
+        <pointLight position={[-30, -20, -20]}/>
+        <Model position= {[-30, -20, -20]} 
+        rotation={[0.2,0,0]} 
+        scale={[2,2,2]}/>
       </Canvas>
     </>
   )
